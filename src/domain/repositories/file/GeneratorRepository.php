c36aa141fb445818bfd8688a2d192dd43507ca79
Vitaliy <theyamshikov@yandex.ru>
2019-01-13 01:41:23 +0600

    refacto

--- src/domain/repositories/file/GeneratorRepository.php
+++ src/domain/repositories/file/GeneratorRepository.php
@@ -2,6 +2,7 @@
 
 namespace yii2module\vendor\domain\repositories\file;
 
+use yii2lab\extension\scenario\collections\ScenarioCollection;
 use yii2lab\extension\scenario\helpers\ScenarioHelper;
 use yii2lab\domain\repositories\BaseRepository;
 
@@ -20,7 +21,8 @@ class GeneratorRepository extends BaseRepository {
 	 */
 	public function generate($config, $name) {
 		$config['class'] = self:: GENERATOR_DIR. $name;
-		return ScenarioHelper::run($config);
+		$filterCollection = new ScenarioCollection($config);
+		return $filterCollection->runAll();
 	}
 	
 	/**
@@ -33,7 +35,8 @@ class GeneratorRepository extends BaseRepository {
 	 */
 	public function install($config, $name) {
 		$config['class'] = self::INSTALL_DIR . $name;
-		return ScenarioHelper::run($config);
+		$filterCollection = new ScenarioCollection($config);
+		return $filterCollection->runAll();
 	}
 	
 }
